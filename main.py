92bd01dc0cb949227f26d38922c0942a7370a32b
Francesco Celentano <francesco.celentano19@gmail.com>
2024-12-16 18:41:57 +0100

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-# commento
-print("sto scrivendo del codice python")
-print("modifica")
-print("sisi")
+testo = "Ciao"
+numero = 5
+decimale = 4.9
+booleano = True
+saluto = "ciao"
+destinatario = " a tutti"
+print(saluto + destinatario + "!" * 4)
+pippo = 42
+print(pippo)
+pi = 3.14679743324
+num = 5 * pi
+print(int (num))
